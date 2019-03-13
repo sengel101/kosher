64030207c70500abb45e4d1bd48d8c477fba4d2e
Shloime Engel <sengel613@gmail.com>
2019-03-13 16:32:02 -0400

    Part I changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,38 @@
 import {Component, OnInit} from '@angular/core';
+import {Restaurant} from './models/restaurant';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'kosher';
-  subtitle = 'This is a card';
-  body = 'body ...';
+export class AppComponent {
+    rests: Restaurant[];
 
-  ngOnInit():void {
-    this.title = 'This Word !';
-    this.subtitle = 'This is a modified Subtitle ..';
+    constructor() {
+      this.rests = [];
+      let temp = new Restaurant();
+      temp.name = 'Tav Cafe';
+      temp.phone = '(514) 112-3581';
+      temp.rating = 3;
+      temp.priceRange = 3;
+      temp.website = 'http://www.tav.ca';
+      this.rests.push(temp);
+
+      temp = new Restaurant();
+      temp.name = 'Pizza Pita';
+      temp.phone = '(514) 731-2296';
+      temp.rating = 2;
+      temp.priceRange = 5;
+      temp.website = 'http://www.pizzapita.ca';
+      this.rests.push(temp);
+
+      temp = new Restaurant();
+      temp.name = 'Exceptions';
+      temp.phone = '(514) 555-1212';
+      temp.rating = 5;
+      temp.priceRange = 8;
+      this.rests.push(temp);
+
 }
-
 }
